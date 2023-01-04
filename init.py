65f9a73881748849c691564b49effcc0e30486c3
site-a0408258 <diy5712635@gmail.com>
2023-01-05 05:48:35 +0800

    py

--- init.py
+++ init.py
@@ -6,7 +6,7 @@ import os
 # get current directory, on Windows, back slash at the end of the directory
 _curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
 # config directory
-config_dir = _curdir + "/config/"
+config_dir = _curdir + "/41123243/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
